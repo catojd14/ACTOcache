7cb59b8db4c3023478b6b6e2c08d611cc58a1fe6
Guoyi Xiao <guoyi6901@gmail.com>
2021-03-31 23:57:26 -0400

    Update Cache class

--- cache.php
+++ cache.php
@@ -9,125 +9,136 @@ You need to be able to add to and remove from the cache.
 Users should also be able to set the cache expiry, but your default expiry should be 10 minutes.
  */
 
-class Cache
-{
-	private $cache_path = 'cacche/';
-    private $catch_name = 'default';
-    private $extension = '.cache';
+class Cache {
+    private $_name;
+    private $_dir;
+    private $_extension;
+    private $_path;
 
-    public function __construction($config=null) {
-        if(isset($config)) {
-            if(is_string($config)) {
-                $this->setCache($config);
-            } else if (is_array($config)) {
-                $this->setCache($config['name']);
-                $this->setCachePath($config['path']);
-                $this->setExtension($config['extension']);
+    private $_autoSave = false;
+    private $_cache;
+
+    public function __construct(string $name = "default", string $dir = "tmp/", string $extension = ".cache") {
+
+        $this->_name = $name;
+        $this->_dir = $dir;
+        $this->_extension = $extension;
+        $this->_path = $this->getCachePath();
+
+        $this->loadCache();
+    }
+
+    public function setAutoSave(bool $state) : void {
+
+        $this->_autoSave = $state;
+    }
+
+    public function get(string $key, &$out) : bool {
+
+        if ($this->_cache === null) return false;
+        if (!array_key_exists($key, $this->_cache)) return false;
+
+        $data = $this->_cache[$key];
+
+        if ($this->isExpired($data)) {
+            
+            unset($this->_cache[$key]);
+
+            if ($this->_autoSave) {
+                $this->saveCache();
             }
-        }
-    }
 
-    // Check data with a array
-
-    public function isCached($key) {
-        if($this->loadCache()) {
-            $cachedData = $this->loadCache();
-            return isset($cachedData[$key]['data']);
-        }
-    }
-
-    //Add data in the Cache
-
-    public function store ($key, $data, $expiration = 600) {
-        $storeData = array(
-            'time'  => time(),
-            'expire'=> $expiration,
-            'data'  => serialize($data)
-        );
-        $dataArray = $this->loadCache();
-        if (is_array($dataArray)) {
-            $dataArray[$key] = $storeData;
-        } else {
-            $dataArray = array($key => $storeData);
-        }
-        $cachedData = json_encode($dataArray);
-        file_put_contents($this->getCacheDir(), $cachedData);
-        return $this;
-    }
-
-    //Get cached data with its key
-    public function retrieve($key, $timestamp = false) {
-        $cachedData = $this->loadCache();
-        ($timestamp === false) ? $type = 'data' : $type = 'time';
-
-        if (!isset($cachedData[$key][$type])) return null;
-
-        return unserialize($cachedData[$key][$type]);
-    }
-
-    //Load cache
-
-    private function loadCache() {
-        if(file_exists($this->getCacheDir())) {
-            $file = file_get_contents($this->getCacheDir());
-            return json_decode($file, true);
-        } else {
             return false;
         }
+
+        $out = unserialize($data["v"]);
+        return true;
     }
 
-    //Remove cached data by its key
-    public function remove($key) {
-        $cachedData = $this->loadCache();
-        if(is_array($cachedData)) {
-            if (isset($cachedData[$key])) {
-                unset($cachedData[$key]);
-                $cachedData = json_encode($cachedData);
-                file_put_contents($this->getCacheDir(), $cachedData);
-            } else {
-                throw new Exception("Error: erase() - Key '{$key}' not found.");
-            }
+    public function set(string $key, $value, int $ttl = 600) : void {
+
+        $data = [
+            "t" => time(),
+            "e" => $ttl,
+            "v" => serialize($value),
+        ];
+
+        if ($this->_cache === null) {
+            $this->_cache = [
+                $key => $data,
+            ];
+        }
+        else {
+            $this->_cache[$key] = $data;
+        }
+
+        if ($this->_autoSave) {
+            $this->saveCache();
         }
     }
 
-    // Get the cache directory path
-    public function getCacheDir() {
-      $filename = $this->getCache();
-      $filename = preg_replace('/[^0-9a-z\.\_\-]/i', '', strtolower($filename));
+    public function delete(string $key) : bool {
 
-      return $this->getCachePath() . $filename . $this->getExtension();
-    
+        if ($this->_cache === null) return false;
+        if (!array_key_exists($key, $this->_cache)) return false;
+
+        unset($this->_cache[$key]);
+
+        if ($this->_autoSave) {
+            $this->saveCache();
+        }
+
+        return true;
     }
 
-    //Cache Path Setter
-    public function setCachePath($path) {
-    $this->cache_path = $path;
-    return $this;
-   }
+    public function deleteExpired() : bool {
 
-   // catch path getter
-   public function getCachePath() {
-       return $this->cache_path;
-   }
+        if ($this->_cache === null) return false;
 
-   //Catch name setter
-   public function setCache($name) {
-       $this->cache_name;
-       return $this;
-   }
+        foreach ($this->_cache as $key => $value) {
+            if($this->isExpired($value)) {
+                unset($this->_cache[$key]);
+            }
+        }
 
-   //Cache name getter
-   public function getCache() {
-       return $this->cache_name;
-   }
+        if ($this->_autoSave) {
+            $this->saveCache();
+        }
 
-   //Cache extension setter
-   public function setExtension($ext) {
-       $this->extension = $ext;
-       return $this;
-   }
+        return true;
+    }
 
-   //Catch extension getter() {
-       return $this->extension;
-   } 
+    private function isExpired($data) : bool {
+
+        if ($data["e"] == -1) return false;
+
+        $expiresOn = $data["t"] + $data["e"];
+        return $expiresOn < time();
+    }
+
+    public function saveCache() : bool {
+
+        if ($this->_cache === null) return false;
+
+        $content = json_encode($this->_cache);
+        file_put_contents($this->_path, $content);
+
+        return true;
+    }
+
+    public function loadCache() : bool {
+
+        if (!file_exists($this->_path)) return false;
+
+        $content = file_get_contents($this->_path);
+        $this->_cache = json_decode($content, true);
+        
+        return true;
+    }
+
+    private function getCachePath() : string {
+
+        return $this->_dir . md5($this->_name . $this->_salt) . $this->_extension;
+    }
+
 }
